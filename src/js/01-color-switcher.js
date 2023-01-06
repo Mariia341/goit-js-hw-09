f5042252684e1497ae5d45524c02f0f18f74d3ab
Mariia Makhonina <106169802+Mariia341@users.noreply.github.com>
2023-01-06 02:06:18 +0200

    +1.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+const body = document.querySelector("body");
 
+console.log(body);
+let timerId = null;
+
+
+
+startBtn.addEventListener("click", () => {
+startBtn.disabled = true; 
+  timerId = setInterval(() => {
+      body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`
+  }, 1000);
+}) ;
+
+
+stopBtn.addEventListener("click", () => {
+startBtn.disabled = false; 
+  clearInterval(timerId);
+});
